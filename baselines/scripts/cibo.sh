3354dd6da66c1850523271bbe25afb046b595df4
umkiyoung <se99an@gmail.com>
2025-05-14 00:54:56 +0900

    Scripts

--- baselines/scripts/cibo.sh
+++ baselines/scripts/cibo.sh
@@ -1,30 +1,30 @@
 export PYTHONPATH=/home/uky/repos_python/Release/CiBO/baselines:$PYTHONPATH
 
 
-# # ----- Indicator False -----
-# # Ackley
-# for seed in 0 1 2 3; do
-#         CUDA_VISIBLE_DEVICES=$seed python baselines/algorithms/CiBO.py --wandb true --task Ackley --dim 200 --batch_size 100\
-#                 --n_init 200 --max_evals 10000 --seed $seed --proxy_hidden_dim 1024 --prior_hidden_dim 512 --num_proxy_epochs 100 --num_prior_epochs 500 --num_posterior_epochs 50\
-#                 --lamb 10 --buffer_size 3000 --flow_steps 250 --alpha 1e-5 --beta 1 --t_scale 1.0\
-#                 --hidden_dim 256 --s_emb_dim 256 --t_emb_dim 256 --harmonics_dim 256 --gfn_buffer_size 600000 --gfn_batch_size 500\
-#                 --T 50  --pis_architectures --zero_init --clipping --mode_fwd tb  --lr_policy 1e-3 --lr_back 1e-3 --lr_flow 1e-1\
-#                 --both_ways --prioritized rank --rank_weight 1e-2 --indicator false --M 10\
-#         &
-# done
-# wait
+# ----- Indicator False -----
+# Ackley
+for seed in 0 1 2 3; do
+        CUDA_VISIBLE_DEVICES=$seed python baselines/algorithms/CiBO.py --wandb true --task Ackley --dim 200 --batch_size 100\
+                --n_init 200 --max_evals 10000 --seed $seed --proxy_hidden_dim 1024 --prior_hidden_dim 512 --num_proxy_epochs 100 --num_prior_epochs 500 --num_posterior_epochs 50\
+                --lamb 10 --buffer_size 3000 --flow_steps 250 --alpha 1e-5 --beta 1 --t_scale 1.0\
+                --hidden_dim 256 --s_emb_dim 256 --t_emb_dim 256 --harmonics_dim 256 --gfn_buffer_size 600000 --gfn_batch_size 500\
+                --T 50  --pis_architectures --zero_init --clipping --mode_fwd tb  --lr_policy 1e-3 --lr_back 1e-3 --lr_flow 1e-1\
+                --both_ways --prioritized rank --rank_weight 1e-2 --indicator false --M 10\
+        &
+done
+wait
 
-# # Rastrigin
-# for seed in 0 1 2 3; do
-#         CUDA_VISIBLE_DEVICES=$seed python baselines/algorithms/CiBO.py --wandb true --task Rastrigin --dim 200 --batch_size 100\
-#                 --n_init 200 --max_evals 10000 --seed $seed --proxy_hidden_dim 1024 --prior_hidden_dim 512 --num_proxy_epochs 100 --num_prior_epochs 500 --num_posterior_epochs 50\
-#                 --lamb 10 --buffer_size 2000 --flow_steps 250 --alpha 1e-5 --beta 1 --t_scale 1.0\
-#                 --hidden_dim 256 --s_emb_dim 256 --t_emb_dim 256 --harmonics_dim 256 --gfn_buffer_size 600000 --gfn_batch_size 500\
-#                 --T 50  --pis_architectures --zero_init --clipping --mode_fwd tb  --lr_policy 1e-3 --lr_back 1e-3 --lr_flow 1e-1\
-#                 --both_ways --prioritized rank --rank_weight 1e-2 --indicator false --M 10\
-#         &
-# done
-# wait
+# Rastrigin
+for seed in 0 1 2 3; do
+        CUDA_VISIBLE_DEVICES=$seed python baselines/algorithms/CiBO.py --wandb true --task Rastrigin --dim 200 --batch_size 100\
+                --n_init 200 --max_evals 10000 --seed $seed --proxy_hidden_dim 1024 --prior_hidden_dim 512 --num_proxy_epochs 100 --num_prior_epochs 500 --num_posterior_epochs 50\
+                --lamb 10 --buffer_size 2000 --flow_steps 250 --alpha 1e-5 --beta 1 --t_scale 1.0\
+                --hidden_dim 256 --s_emb_dim 256 --t_emb_dim 256 --harmonics_dim 256 --gfn_buffer_size 600000 --gfn_batch_size 500\
+                --T 50  --pis_architectures --zero_init --clipping --mode_fwd tb  --lr_policy 1e-3 --lr_back 1e-3 --lr_flow 1e-1\
+                --both_ways --prioritized rank --rank_weight 1e-2 --indicator false --M 10\
+        &
+done
+wait
 
 #Rosenbrock
 for seed in 0 1 2 3; do
